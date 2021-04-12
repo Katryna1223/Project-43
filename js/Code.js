65ace7b5b18ba03a88ffd956fbf92b6ade0ace3d
Katryna1223 <75594467+Katryna1223@users.noreply.github.com>
2021-04-12 00:14:00 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    text("A T A D E S B A", 400, 200);
+    text("Hint: Everyone can play from anywhere!", 400, 220);
+
+    text("C U T N I F O N", 100, 285);
+    text("Hint: Tells the computer what to do.", 100, 305);
+
+}
